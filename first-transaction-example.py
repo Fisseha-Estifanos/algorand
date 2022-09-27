2d2107373ac770f08bb791c49def445590f77b6d
Fisseha Linux <fisseha.137@gmail.com>
2022-09-27 13:51:05 +0300

    refactoring

--- first-transaction-example.py
+++ first-transaction-example.py
@@ -5,6 +5,8 @@ from algosdk import account, mnemonic, constants
 from algosdk.v2client import algod
 from algosdk.future import transaction
 from dotenv import load_dotenv
+from transaction_helpers import get_mnemonic, get_balance
+from transaction_helpers import commit_transaction, generate_algorand_key_pair
 load_dotenv()
 
 wallet_1 = os.getenv('my_address_w1')
@@ -16,18 +18,19 @@ key_3 = os.getenv('private_key_w3')
 test_address = os.getenv('test_address')
 test_key = os.getenv('test_private_key')
 
-
 # Write down the address, private key, and the passphrase for later usage
 # generate_algorand_key_pair()
 
 # get_mnemonic(test_key)
 
-
-
-
-
 # replace private_key and sender_address with your private key and sender
 # address.
 # Send from wallet_1 ---> wallet_3 in this case
-first_transaction_example(key_1, test_address, wallet_3)
+# commit_transaction(key_1, test_address, wallet_3)
+
+# check account balance
+get_balance(test_address)
+get_balance(wallet_1)
+get_balance(wallet_3)
+
 print('--- over and out ---')
