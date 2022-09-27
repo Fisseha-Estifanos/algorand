6c24c1501c08ef2bad95d6d4dc23a04c087f21be
Fisseha Linux <fisseha.137@gmail.com>
2022-09-27 14:26:23 +0300

    proper rename

--- make_transaction.py
+++ make_transaction.py
@@ -9,13 +9,15 @@ from transaction_helpers import get_mnemonic, get_balance
 from transaction_helpers import commit_transaction, generate_algorand_key_pair
 load_dotenv()
 
+# DEV NET ADDRESSES
 wallet_1 = os.getenv('my_address_w1')
 key_1 = os.getenv('private_key_w1')
-
 wallet_3 = os.getenv('my_address_w3')
 key_3 = os.getenv('private_key_w3')
 
+# TEST NET ADDRESSES
 test_address = os.getenv('test_address')
+to_address = os.getenv('and_address')
 test_key = os.getenv('test_private_key')
 
 # Write down the address, private key, and the passphrase for later usage
@@ -26,11 +28,11 @@ test_key = os.getenv('test_private_key')
 # replace private_key and sender_address with your private key and sender
 # address.
 # Send from wallet_1 ---> wallet_3 in this case
-# commit_transaction(key_1, test_address, wallet_3)
+commit_transaction(key_1, test_address, to_address)
 
 # check account balance
-get_balance(test_address)
-get_balance(wallet_1)
-get_balance(wallet_3)
+# get_balance(test_address)
+# get_balance(wallet_1)
+# get_balance(wallet_3)
 
 print('--- over and out ---')
