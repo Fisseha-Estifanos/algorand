1f2809bf4ad4bb1c24e25fe7468b4f0f579b4bfb
Fisseha Linux <fisseha.137@gmail.com>
2022-09-27 13:50:59 +0300

    adding default values holder script

---
+++ defaults.py
@@ -0,0 +1,9 @@
+"""
+A script to store default values
+"""
+
+# ip address of the network
+algorand_ip_address = "http://localhost:4001"
+
+# token of the network
+algorand_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
