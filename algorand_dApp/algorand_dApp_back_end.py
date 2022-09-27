098f547c5c42161dc98ab84d229390aac15f6c1e
Fisseha Linux <fisseha.137@gmail.com>
2022-09-27 21:19:02 +0300

    minor refactors

--- algorand_dApp/algorand_dApp_back_end.py
+++ algorand_dApp/algorand_dApp_back_end.py
@@ -1,3 +1,4 @@
+import sqlite3
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,3 +12,17 @@ def transactions():
 @app.route('/certificates')
 def certificates():
     return render_template('certificates.html')
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+@app.route('/index')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
