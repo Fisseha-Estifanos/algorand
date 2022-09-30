09b14f203b08ed1fa1c187b94089c5b62a10279d
Fisseha Linux <fisseha.137@gmail.com>
2022-09-30 17:00:42 +0300

    added more address

--- scripts/defaults.py
+++ scripts/defaults.py
@@ -8,3 +8,16 @@ algorand_test_ip_address = "https://testnet-api.algonode.cloud"
 
 # token of the network
 algorand_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+
+# test addresses
+base_test_address = 'WF7P5H6IJM34MWAQYDNNKADTPHPB3AUZAEMEZHWCI6OT7C46XGRQCWJJGU'
+second_test_address = 'TF2JE6IK77FFWJPIGNZRTDH2VHULUCWAGQ3M53Q6VACHNJSEM5YBAFF4NA'
+
+andinet_test_address = 'GBWDIFQEQDNZ5JZT3CZKDKLLS6YBGUWAOZLRO5GUOH35QXFIP3AMNVGKBA'
+nati_test_address = '53S7TO35ACZJKAA7NRDKRIZ5EAQGIVYVHQXLBWOALGCSUBIX4AWD5TCS6E'
+
+
+# dev network addersses
+my_address_w1 = 'OMT4JDUEYPFMJTJG5BLTBWFKXHP3XILKIT76BXAKSZF4JXM7VCM2Q7MIFY'
+my_address_w3 = 'FESYQ3KZ7J3SPRFYW4RD3NVJZ3VECXUD2JR4AEKISR7SJURIJL3IHA746U'
