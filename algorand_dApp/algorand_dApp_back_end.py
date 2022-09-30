ea46e2300c499f33d57dd45a48e6730f79beb6ce
Fisseha Linux <fisseha.137@gmail.com>
2022-09-30 16:38:40 +0300

    making transactions completed

--- algorand_dApp/algorand_dApp_back_end.py
+++ algorand_dApp/algorand_dApp_back_end.py
@@ -1,5 +1,5 @@
 # import jsonify
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, request, render_template, url_for, flash, redirect
 from werkzeug.exceptions import abort
 import os
 import sys
@@ -10,15 +10,20 @@ sys.path.append('..')
 sys.path.insert(1, '/scripts/')
 from scripts.transaction_helpers import *
 import scripts.defaults as defs
-
 load_dotenv()
+
+
 wallet_1 = os.getenv('my_address_w1')
+flask_secret_key = os.getenv('flask_secret_key')
 
 app = Flask(__name__)
-app.secret_key = os.urandom(12).hex()
+app.secret_key = flask_secret_key
 
 
 def get_db_connection():
+    """
+    A function to create a sqlite db connection
+    """
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
@@ -40,8 +45,12 @@ def post(post_id):
     return render_template('post.html', post=post)
 """
 
+
 @app.route('/index')
 def index():
+    """
+    The main index page
+    """
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
@@ -50,24 +59,90 @@ def index():
 
 @app.route('/certificates')
 def certificates():
+    """
+    The main certificates page
+    """
     return render_template('certificates.html')
 
 
-@app.route('/transactions')
+@app.route('/transactions', methods=['GET'])
 def transactions():
-    return render_template('transactions.html')
+    """
+    The main transactions page
+    """
+    account_balance = 0
+    return render_template('transactions.html', balance=account_balance)
+
+
+@app.route('/transactions/display_records', methods=['GET'])
+def display_records():
+    """
+    The display records endpoint
+    """
+    account_balance = 0
+    return render_template('transactions.html', balance=account_balance)
 
 
 @app.route('/transactions/display_balance', methods=['GET', 'POST'])
 def display_balance():
-    print('in get')
-    account = request.form['address']
-    # account = request.data
-    print(f"loading the account: {account}'s balance")
-    if not account:
-        flash('Account address is required')
-    else:
-        print(f"loading the account: {account}'s balance")
-        account_balance = 888
-        return render_template('transactions.html',
-                               balance=account_balance)
+    """
+    The display balance endpoint
+    """
+    account_balance = 0
+    if request.method == "POST":
+        print('In post')
+        account = request.form.get("accountAddress")
+        if not account:
+            flash('Account address is required')
+            return render_template('transactions.html',
+                                   balance=account_balance)
+        else:
+            print(f"loading the balance of the account: '{account}'")
+            # calling the get_balance function to get the balance of the given
+            # account
+            account_balance = get_balance(account)
+            print(f"displaying the balance of the account: '{account}'")
+            return render_template('transactions.html',
+                                   balance=account_balance)
+    print('In get')
+    return render_template('transactions.html', balance=account_balance)
+
+
+@app.route('/transactions/commit_transaction', methods=['GET', 'POST'])
+def commit_transaction():
+    """
+    The make transactions endpoint
+    """
+    if request.method == "POST":
+        print('In post')
+        sender_address = request.form.get('fromAddress')
+        receiver_address = request.form.get('toAddress')
+        algo_amount = request.form.get('algo-amount')
+        algo_amount = int(algo_amount)
+        if not sender_address:
+            flash('Sender account address is required')
+            return render_template('transactions.html', balance=0)
+        if not receiver_address:
+            flash('Receiver account address is required')
+            return render_template('transactions.html', balance=0)
+        if not algo_amount:
+            flash('Algo amount to send is required')
+            return render_template('transactions.html', balance=0)
+
+        print(f'sender address: {sender_address}\nreceiver address: '
+              f'{receiver_address}\nalgo amount: {algo_amount}')
+        pk = os.getenv('test_private_key')
+        sent = make_transaction(pk, sender_address, receiver_address,
+                                algo_amount)
+        if sent:
+            flash('Transaction successfully committed to the Algorand '
+                  + 'blockchain')
+            return render_template('transactions.html', balance=0)
+        else:
+            flash('Transaction failed, see log for more details')
+            return render_template('transactions.html', balance=0)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
