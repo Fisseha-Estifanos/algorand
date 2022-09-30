5618ef0032dd84bf2df5580fcd04652b202bba96
Fisseha Linux <fisseha.137@gmail.com>
2022-09-30 16:39:15 +0300

    added test account addresses

--- scripts/defaults.py
+++ scripts/defaults.py
@@ -2,7 +2,7 @@
 A script to store default values
 """
 
-# ip address of the network
+# ip address of algorand networks
 algorand_local_ip_address = "http://localhost:4001"
 algorand_test_ip_address = "https://testnet-api.algonode.cloud"
 
