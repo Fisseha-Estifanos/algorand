b959f9af4172e1cdeb4b1021bdc333f7dc809aca
Fisseha Linux <fisseha.137@gmail.com>
2022-10-02 03:23:45 +0300

    adding mainnet API address

--- scripts/defaults.py
+++ scripts/defaults.py
@@ -5,6 +5,7 @@ A script to store default values
 # ip address of algorand networks
 algorand_local_ip_address = "http://localhost:4001"
 algorand_test_ip_address = "https://testnet-api.algonode.cloud"
+algorand_main_ip_address = "https://mainnet-api.algonode.cloud"
 
 # token of the network
 algorand_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
